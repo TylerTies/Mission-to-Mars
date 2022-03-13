94ddb1ebe222c438629230e88065600f9e027109
TylerTies <tyler.ties@gmail.com>
2022-03-13 03:54:30 -0500

    Completed Deliverables 1&2

--- scraping.py
+++ scraping.py
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": mars_images(browser)
     }
 
     # Stop webdriver and return data
@@ -96,6 +97,40 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def mars_images(browser):
+    # Visit URL
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+
+    # Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # Retrieve the image urls and titles for each hemisphere.
+    html = browser.html
+    link_soup = soup(html, 'html.parser')
+
+    links = link_soup.find_all('img', class_='thumb')
+
+
+    for link in range(len(links)):
+        hemispheres = {}
+
+        browser.find_by_css('div.description a.itemLink.product-item h3')[link].click()
+        
+        
+        img_url = browser.links.find_by_text('Sample')['href']
+        title = browser.find_by_css('h2.title').text
+        
+        hemispheres['img_url'] = img_url
+        hemispheres['title'] = title
+        
+        hemisphere_image_urls.append(hemispheres)
+        
+        browser.back()
+
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
